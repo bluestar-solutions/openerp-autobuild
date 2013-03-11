574bd0740bd595f0e2abcced336cbbd8e4a98381
Hervé Martinet <herve.martinet@blues2.ch>
2013-03-11 14:37:32 +0100

    Fixes

--- make.sh
+++ make.sh
@@ -1,13 +1,14 @@
 #/bin/sh
 
 TARGET_DIR=target
+VERSION=$(git describe --exact-match --abbrev=0)
 
 rm -Rf $TARGET_DIR
-mkdir -p $TARGET_DIR
-cp -R debian $TARGET_DIR/
-cp openerp-autobuild.py $TARGET_DIR/openerp-autobuild
+mkdir -p $TARGET_DIR/openerp-autobuild
+cp -R debian $TARGET_DIR/openerp-autobuild/
+cp openerp-autobuild.py $TARGET_DIR/openerp-autobuild/openerp-autobuild
 
-cd $TARGET_DIR/
-dch -i
-debuild --no-tgz-check
+cd $TARGET_DIR/openerp-autobuild
+dch --create -v $VERSION --package openerp-autobuild
+debuild -i -us -uc -b
 
