660a5450675e2c271334393b0362aff99e4576df
Hervé Martinet <herve.martinet@blues2.ch>
2013-04-19 10:05:20 +0200

    Fix dependencies

--- make.sh
+++ make.sh
@@ -6,7 +6,7 @@ VERSION=$(git describe --abbrev=0 --tags)
 rm -Rf $TARGET_DIR
 mkdir -p $TARGET_DIR/openerp-autobuild
 cp -R debian $TARGET_DIR/openerp-autobuild/
-cp openerp-autobuild.py $TARGET_DIR/openerp-autobuild/openerp-autobuild
+cp openerp-autobuild.py $TARGET_DIR/openerp-autobuild/openerp-autobuild.py
 cp oebuild_logger.py $TARGET_DIR/openerp-autobuild/oebuild_logger.py
 cp oebuild_conf_schema.py $TARGET_DIR/openerp-autobuild/oebuild_conf_schema.py
 
