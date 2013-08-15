e4f8ee2361f07fca72773a61982cb5599fe28756
Hervé Martinet <herve.martinet@blues2.ch>
2013-08-15 18:11:41 +0200

    Adapt 1.5 deb

--- make.sh
+++ make.sh
@@ -1,20 +1,14 @@
 #/bin/sh
 
 TARGET_DIR=target
-VERSION=$(git describe --abbrev=0 --tags)
+VERSION=1.5-1
 
 rm -Rf $TARGET_DIR
 mkdir -p $TARGET_DIR/openerp-autobuild
 cp -R debian $TARGET_DIR/openerp-autobuild/
-cp openerp-autobuild.py $TARGET_DIR/openerp-autobuild/openerp-autobuild.py
-cp openerp-autobuild-1.5.py $TARGET_DIR/openerp-autobuild/openerp-autobuild-1.5.py
-cp oebuild_logger.py $TARGET_DIR/openerp-autobuild/oebuild_logger.py
-cp oebuild_conf_schema.py $TARGET_DIR/openerp-autobuild/oebuild_conf_schema.py
-cp oebuild_conf_schema_1_5.py $TARGET_DIR/openerp-autobuild/oebuild_conf_schema_1_5.py
-cp oebuild_config.default $TARGET_DIR/openerp-autobuild/oebuild_config.default
-cp oe_config.default $TARGET_DIR/openerp-autobuild/oe_config.default
+cp src/* $TARGET_DIR/openerp-autobuild/
 
 cd $TARGET_DIR/openerp-autobuild
-dch --create -v $VERSION --package openerp-autobuild
+dch --create -v $VERSION --package openerp-autobuild-1-5
 debuild -i -us -uc -b
 
