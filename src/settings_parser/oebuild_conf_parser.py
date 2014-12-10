b8def839e389498b07deac928da5f036cd1007b4
Patrick Tombez <patrick.tombez@blues2.ch>
2014-12-10 15:41:00 +0100

    Update migration procdedure from bzr to git

--- src/settings_parser/oebuild_conf_parser.py
+++ src/settings_parser/oebuild_conf_parser.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP Autobuild
 #    Copyright (C) 2012-2013 Bluestar Solutions Sàrl (<http://www.blues2.ch>).
 #
@@ -15,7 +15,7 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
@@ -38,14 +38,14 @@ class OEBuildConfParser():
     _logger = logging.getLogger(__name__)
     _analyze = False
     params = None
-    
+
     def __init__(self, params, analyze=False):
         self._analyze = analyze
         self.params = params
         for dfile in static_params.DEPRECATED_FILES:
             if os.path.exists(dfile):
                 self._logger.warning('File %s is deprecated, you can remove it from the project' % dfile)
-            
+
     def _load_file(self, file_name, strict_mode=True, alt_schema=False):
         conf = None
         with open(file_name, "r") as source_file:
@@ -58,28 +58,28 @@ class OEBuildConfParser():
                 else:
                     self._logger.warning(_ex('%s is not JSON valid and will be ignored' % file_name, e))
                     raise IgnoreSubConf()
-          
+
         if conf.get(schema.OEBUILD_VERSION, None) != static_params.VERSION:
             conf = self._update_file(conf.get(schema.OEBUILD_VERSION, None), file_name, alt_schema)
-        self._validate_conf(conf, file_name, strict_mode, alt_schema)  
-        
+        self._validate_conf(conf, file_name, strict_mode, alt_schema)
+
         return conf
 
-    def _load_alt_file(self, source_conf, file_name):       
+    def _load_alt_file(self, source_conf, file_name):
         conf = self._load_file(file_name, strict_mode=False, alt_schema=True)
         new_conf = dict(source_conf)
-        
+
         for componant in ['server', 'addons', 'web']:
             if componant in conf[schema.OPENERP]:
                 new_conf[schema.OPENERP][componant].update(conf[schema.OPENERP][componant])
-        
+
         for dep2 in conf[schema.DEPENDENCIES]:
             for dep in new_conf[schema.DEPENDENCIES]:
                 if dep[schema.NAME] == dep2[schema.NAME]:
                     dep.update(dep2)
-                    
+
         return new_conf
-        
+
     def _validate_conf(self, conf, file_name, strict_mode=True, alt_schema=False):
         try:
             jsonschema.validate(conf, schema.OEBUILD_ALT_SCHEMA if alt_schema else schema.OEBUILD_SCHEMA)
@@ -90,14 +90,14 @@ class OEBuildConfParser():
             else:
                 self._logger.warning(_ex('%s will be ignored because it is not a valid configuration file' % file_name, e))
                 raise IgnoreSubConf()
-        
-    def load_oebuild_config_file(self, conf_file_list):   
+
+    def load_oebuild_config_file(self, conf_file_list):
         if not (os.path.exists(static_params.PROJECT_CONFIG_FILE) and os.path.isfile(static_params.PROJECT_CONFIG_FILE)):
             self._logger.error('The project configuration does not exist : %s' % static_params.PROJECT_CONFIG_FILE)
             sys.exit(1)
-            
+
         conf = self._load_file(static_params.PROJECT_CONFIG_FILE)
-            
+
         for conf_name in conf_file_list:
             conf_file_name = static_params.PROJECT_ALT_CONFIF_FILE_PATTERN % conf_name
             if os.path.exists(conf_file_name) and os.path.isfile(conf_file_name):
@@ -105,18 +105,18 @@ class OEBuildConfParser():
                     conf = self._load_alt_file(conf, conf_file_name)
                 except IgnoreSubConf:
                     pass
-            
+
         return conf
-    
+
     def load_transitive_oebuild_config_file(self, conf_file_path, conf_file_list):
         conf_file = '%s/%s' % (conf_file_path, static_params.PROJECT_CONFIG_FILE)
         if not (os.path.exists(conf_file) and os.path.isfile(conf_file)):
             # Probably not an oebuild project
             self._logger.info('%s : %s file not found. It is probably not a oebuild module' % (conf_file_path, static_params.PROJECT_CONFIG_FILE))
             raise IgnoreSubConf()
-        
+
         conf = self._load_file(conf_file)
-                
+
         for conf_name in conf_file_list:
             conf_file = '%s/%s' % (conf_file_path, static_params.PROJECT_ALT_CONFIF_FILE_PATTERN % conf_name)
             if os.path.exists(conf_file) and os.path.isfile(conf_file):
@@ -124,35 +124,35 @@ class OEBuildConfParser():
                     conf = self._load_alt_file(conf, conf_file)
                 except IgnoreSubConf:
                     pass
-                
+
         return conf
-    
-    def create_oebuild_config_file(self, default_serie):      
+
+    def create_oebuild_config_file(self, default_serie):
         overwrite = "no"
         if os.path.exists(static_params.PROJECT_CONFIG_FILE):
-            overwrite = dialogs.query_yes_no("%s file already exists, overwrite it with default one ?" % static_params.PROJECT_CONFIG_FILE, overwrite)   
+            overwrite = dialogs.query_yes_no("%s file already exists, overwrite it with default one ?" % static_params.PROJECT_CONFIG_FILE, overwrite)
         if os.path.exists(static_params.PROJECT_CONFIG_FILE) and overwrite == "no":
             return
-            
+
         with open(static_params.DEFAULT_PROJECT_CONFIG_FILE, 'r') as f:
             content = f.read()
 
         content = re.sub(r'\$OEBUILD_VERSION', static_params.VERSION, content)
         content = re.sub(r'\$PROJECT_NAME', os.getcwd().split('/')[-1], content)
         content = re.sub(r'\$SERIE', default_serie, content)
-            
-        with open(static_params.PROJECT_CONFIG_FILE, 'w+') as f:     
+
+        with open(static_params.PROJECT_CONFIG_FILE, 'w+') as f:
             f.write(content)
-            
+
     def _update_file(self, version_from, file_name, alt_schema=False):
-        self._logger.info(('%s is in version %s and openerp-autobuild is in version %s') % 
+        self._logger.info(('%s is in version %s and openerp-autobuild is in version %s') %
                              (file_name, version_from, static_params.VERSION))
-        
+
         valid_keys = self.UPDATE_ALT_FROM.keys() if alt_schema else self.UPDATE_FROM.keys()
         if version_from not in valid_keys:
             self._logger.warning("Cannot update from version %s: %s will be ignored" % (version_from, file_name))
             raise IgnoreSubConf()
-        
+
         updated_fname = file_name + ".updated"
         with open(file_name, "r") as source_file:
             with open(updated_fname, "w+") as updated_file:
@@ -161,10 +161,10 @@ class OEBuildConfParser():
                 for pattern, replace in regs:
                     content = re.sub(pattern, replace, content)
                 updated_file.write(content)
-                
+
         answer = dialogs.ANSWER_NO
         if len(file_name.split('/')) == 1 and not self._analyze:
-            answer = dialogs.query_yes_no("Do you want to replace the project configuration files by the updated version " + 
+            answer = dialogs.query_yes_no("Do you want to replace the project configuration files by the updated version " +
                                           "(otherwise the updated versions will be saved to new files) ?", default=dialogs.ANSWER_NO)
         if answer == dialogs.ANSWER_YES:
             os.rename(updated_fname, file_name)
@@ -172,16 +172,17 @@ class OEBuildConfParser():
             self._logger.info('%s has been updated to version %s' % (file_name, static_params.VERSION))
         else:
             self._logger.info('An updated version of %s will be used and stored in %s' % (file_name, updated_fname))
-            
+
         with open(updated_fname, "r") as updated_file:
             return json.load(updated_file)
-    
-    UPDATE_FROM =  {
+
+    UPDATE_FROM = {
         "1.7": [jre.update_version("1.7", "2.1"),
-                (r'(\n?)(\s*)("dependencies"\s*:)', r'\1\2"python-dependencies": [],\1\2\3')]
+                (r'(\n?)(\s*)("dependencies"\s*:)', r'\1\2"python-dependencies": [],\1\2\3'),
+                jre.remove_param_array('server'), jre.remove_param_array('addons'), jre.remove_param_array('web')]
     }
-    
-    UPDATE_ALT_FROM =  {
+
+    UPDATE_ALT_FROM = {
         "1.7": [jre.update_version("1.7", "2.1"),
                 jre.remove_param("project"),
                 jre.remove_param("serie")]
--- src/settings_parser/user_conf_1_7_update.py
+++ src/settings_parser/user_conf_1_7_update.py
@@ -26,8 +26,8 @@ import json_regex as jre
 
 USER_OEBUILD_CONFIG_FILE_1_7 = lambda params: '%s/oebuild_config-1.7.json' % params.USER_CONFIG_PATH
 
-def update_from_1_7(conf, conf_1_7, params):    
-    
+def update_from_1_7(conf, conf_1_7, params):
+
     with open(params.USER_CONFIG_FILE) as f:
         content = f.read()
 
@@ -48,16 +48,7 @@ def update_from_1_7(conf, conf_1_7, params):
         for default_serie in conf[user_conf_schema.OPENERP]:
             default_series[default_serie[user_conf_schema.SERIE]] = default_serie
         for serie_1_7 in conf_1_7.get(user_conf_schema.OPENERP, []):
-            if serie_1_7[user_conf_schema.SERIE] in default_series.keys():
-                default_serie = default_series[serie_1_7[user_conf_schema.SERIE]]
-                new_serie = []
-                for elem in ['server', 'addons', 'web']:
-                    if serie_1_7[elem] != default_serie[elem]:
-                        new_serie.append((elem, serie_1_7[elem]))
-                if len(new_serie) > 0:
-                    new_serie.insert(0, (user_conf_schema.SERIE, serie_1_7[user_conf_schema.SERIE]))
-                    new_series.append(new_serie)
-            else:
+            if serie_1_7[user_conf_schema.SERIE] not in default_series.keys():
                 new_series.append(serie_1_7)
         if new_series:
             oe_content = ''
@@ -65,7 +56,11 @@ def update_from_1_7(conf, conf_1_7, params):
             for new_serie in new_series:
                 oe_content += '%s{' % ('' if first_serie else ',')
                 first_elem = True
-                for key, val in new_serie:
+                for key, val in new_serie.iteritems():
+                    if key in ['server', 'addons', 'web']:
+                        continue
+                    if key == 'serie' and val == 'trunk':
+                        val = 'master'
                     oe_content += '%s%s"%s": "%s"' % ('\n' if first_elem else ',\n', ' ' * 8, key, val)
                     first_elem = False
                 oe_content += '\n%s}' % (' ' * 4)
@@ -85,7 +80,7 @@ def update_from_1_7(conf, conf_1_7, params):
             if db_conf[user_conf_schema.PASSWORD] != db_conf_1_7.get(user_conf_schema.PASSWORD, db_conf[user_conf_schema.PASSWORD]):
                 db_content.append('%s"%s": "%s"' % (' ' * 8, user_conf_schema.PASSWORD, db_conf_1_7[user_conf_schema.PASSWORD]))
         if db_content:
-            content = re.sub(r'(\n?)(\s*)("%s"\s*:[\s|\n]*)(\{[\s|\n]*\})' % user_conf_schema.DATABASE, 
+            content = re.sub(r'(\n?)(\s*)("%s"\s*:[\s|\n]*)(\{[\s|\n]*\})' % user_conf_schema.DATABASE,
                              r'\1\2\3{\n%s\2}' % (',\n'.join(db_content) + '\n'), content)
 
         f.write(content)
